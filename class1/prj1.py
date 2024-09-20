11e3029b09115e393a21012d86c68f06024566d9
Andre-Wang111 <andrewang0729@gmail.com>
2024-09-20 20:09:35 +0800

    訂正hw1

---
+++ class1/prj1.py
@@ -0,0 +1,11 @@
+print("I love Python!")
+# basic types
+print(1)  # 整数 int
+print(1.0)  # 浮點數 float只要有小數點就是浮點數
+print("hello")  # 字符串 str,用雙引號""或者單引號''包起來的都是字串
+print(True)  # 布林值 bool,第一個字母要大寫
+print(None)  # 空值 NoneType
+# 變數 variable
+a = 1  # 把1這個整數存到電腦的記憶體中
+# = 的功能是將右邊的資料存到左邊的變數空間當中
+print(a)  # 把a這個變數的值印出來
