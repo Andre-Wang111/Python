a1cc5e545be63fe33e4b9ff6917eceb702efcb17
Andre-Wang111 <andrewang0729@gmail.com>
2024-09-20 20:33:18 +0800

    class2

--- class2/class.py
+++ class2/class.py
@@ -1,1 +1,24 @@
 print("hello")
+# 字串運算
+a = "hello"
+b = "world"
+c = a + " " + b  # 字串相加
+print(c)
+
+a = a * 3  # 字串乘法
+print(a)
+
+# 認識基本指令
+# 指令會有名稱和括號組成，()裡面放提供給指令的參數
+# 每個參數都要用逗號隔開
+m = max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)  # 取最大值
+print(m)
+
+le = len("hello")  # 取字串長度
+print(le)
+
+
+print(type(1))  # 取型態
+print(type("hello"))  # 取型態
+print(type(1.0))  # 取型態
+print(type(True))  # 取型態
