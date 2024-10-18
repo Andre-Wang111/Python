4a53df870baee231b6cd225b30df784e651cf91d
Andre-Wang111 <andrewang0729@gmail.com>
2024-10-18 18:25:39 +0800

    class5

--- class5/hw.py
+++ class5/hw.py
@@ -3,3 +3,17 @@
 讓turtle蓋出一個時鐘數字的位置
 總共會蓋出12個印章
 """
+
+import turtle as t
+
+
+t.penup()
+t.speed(0)
+for i in range(12):
+    t.forward(100)
+    t.stamp()
+    t.backward(100)
+    t.left(30)
+
+
+t.done()
