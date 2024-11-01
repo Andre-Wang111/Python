ad0729f80c2c5ae9c52c0683d62e6832667f1a3b
Andre-Wang111 <andrewang0729@gmail.com>
2024-11-01 18:46:08 +0800

    class7

--- class7/hw.py
+++ class7/hw.py
@@ -27,3 +27,27 @@ EX:
   *  
   * 
 """
+
+# 輸入一數字 n
+n = int(input("請輸入正整數: "))
+
+# 尋找3和7的倍數
+print("3和7的倍數:")
+for i in range(1, n + 1):
+    if i % 3 == 0 or i % 7 == 0:
+        print(i)
+
+# 輸入箭頭大小
+arrow_size = int(input("請輸入要印出的箭頭大小: "))
+
+# 印出箭頭
+for i in range(arrow_size):
+    # 打印上半部分
+    spaces = " " * (arrow_size - i - 1)
+    stars = "*" * (2 * i + 1)
+    print(spaces + stars)
+
+for i in range(arrow_size):
+    # 打印下半部分
+    spaces = " " * (arrow_size - 1)
+    print(spaces + "*")
