5cddc56d9c1f13729cc78df6d68f4932e63fa3d0
Andre-Wang111 <andrewang0729@gmail.com>
2025-01-03 20:34:51 +0800

    class17

--- class17/class.py
+++ class17/class.py
@@ -12,3 +12,37 @@ print(a)   # 顯示 10
 
 
 
+# def doc 用途
+def add(a, b):
+    """
+
+    加法函式
+    :param a: 數字1
+    :param b: 數字2
+    :return:  a+b
+
+    """
+    return a + b
+
+
+
+# use help 函式可以查看函式的說明文件
+help(add)
+
+
+# use_doc_屬性也可以查看函式的說明文件
+print(add.__doc__)
+print(add.__name__)
+
+
+# eval(input()) - 這個函式可以讓使用者輸入的文字變成程式碼執行
+ans = eval(" 5 + 3 ")
+print(ans)
+
+
+# 這樣就可以讓 user 輸入數學運算式，然後計算結果
+ans = eval(input("請輸入數學運算式:"))
+print(ans)
+# 如果輸入:
+# 5 + 3
+# 顯示8
